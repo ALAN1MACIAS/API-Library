7c28130a99aba14b6676e411a9f34e5006d8bcce
Alan Guillermo Avila Macias <g20160094@gmail.com>
2021-07-19 02:12:34 -0500

    [ADD] has_secure_password

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 describe 'Authentication', type: :request do
   describe 'POST /authenticate' do
+    let(:user) { FactoryBot.create(:user, username: 'BookSeller99', password: 'Password1') }
+
     it 'authenticates the client' do
-      post '/api/v1/authenticate', params: { username: 'BookSeller99', password: 'Password1' }
+      post '/api/v1/authenticate', params: { username: user.username, password: 'Password1' }
 
       expect(response).to have_http_status(:created)
-      expect(response_body).to eq({ 'token' => '123' })
+      expect(response_body).to eq({ 'token' => response_body['token'] })
     end
 
     it 'returns error when username is missing' do
@@ -21,6 +23,12 @@ describe 'Authentication', type: :request do
 
       expect(response).to have_http_status(:unprocessable_entity)
       expect(response.body).to eq("{\"error\":\"param is missing or the value is empty: password\"}")
+    end
+
+    it 'returns error when password is incorrect' do
+      post '/api/v1/authenticate', params: { username: user.username, password: 'incorrect' }
+
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
--- spec/services/authentication_token_service_spec.rb
+++ spec/services/authentication_token_service_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe AuthenticationTokenService do
   describe '.call' do
-    let(:token) { described_class.call }
+    let(:token) { described_class.call(1) }
 
     it 'returns an authentication token' do
       decoded_token = JWT.decode token, described_class::HMAC_SECRET, true, { algorithm: described_class::ALGORITHM_TYPE }
 
       expect(decoded_token).to eq(
         [
-          { 'test' => 'test1' },
+          { 'user_id' => 1 },
           { 'alg' => 'HS256' }
         ]
       )
